db237cc07838f8d19f5f04de6e4b676d9ddc4980
Alexandre Parenteau <aubonbeurre@gmail.com>
2018-04-06 14:11:30 +0200

    changes

---
+++ distcc.rb
@@ -0,0 +1,50 @@
+class Distcc < Formula
+  desc "Distributed compiler client and server"
+  homepage "https://github.com/aubonbeurre/homebrew-distcc"
+  url "https://github.com/aubonbeurre/homebrew-distcc/archive/myv3.2rc1.2.zip"
+  version "3.2rc1.2"
+  sha256 ""
+
+  depends_on "autoconf" => :build
+  depends_on "automake" => :build
+
+  def install
+    # Make sure python stuff is put into the Cellar.
+    # --root triggers a bug and installs into HOMEBREW_PREFIX/lib/python2.7/site-packages instead of the Cellar.
+    inreplace "Makefile.in", '--root="$$DESTDIR"', ""
+    system "./autogen.sh"
+    system "./configure", "--prefix=#{prefix}", "--without-libiberty", "--disable-Werror"
+    system "make", "install"
+  end
+
+  plist_options :manual => "distccd"
+
+  def plist; <<~EOS
+    <?xml version="1.0" encoding="UTF-8"?>
+    <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+    <plist version="1.0">
+      <dict>
+        <key>Label</key>
+        <string>#{plist_name}</string>
+        <key>RunAtLoad</key>
+        <true/>
+        <key>KeepAlive</key>
+        <true/>
+        <key>ProgramArguments</key>
+        <array>
+            <string>#{opt_prefix}/bin/distccd</string>
+            <string>--daemon</string>
+            <string>--no-detach</string>
+            <string>--allow=192.168.0.1/24</string>
+        </array>
+        <key>WorkingDirectory</key>
+        <string>#{opt_prefix}</string>
+      </dict>
+    </plist>
+    EOS
+  end
+
+  test do
+    system "#{bin}/distcc", "--version"
+  end
+end
